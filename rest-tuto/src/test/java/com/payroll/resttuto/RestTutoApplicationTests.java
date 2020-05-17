840af43389eb116a1d8034f0ff334db2a7471a2b
CHIHAOUIKAIS <kais.chihaoui@esprit.tn>
2020-05-17 04:03:31 +0200

    spring boot rest api guide

---
+++ rest-tuto/src/test/java/com/payroll/resttuto/RestTutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payroll.resttuto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestTutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
